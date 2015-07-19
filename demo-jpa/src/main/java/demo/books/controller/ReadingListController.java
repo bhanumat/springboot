60b3d4500f1ad7cd20550190c0ca2bb804ac6423
bhanumat <bhanumat.w@gmail.com>
2015-07-19 15:20:15 +0700

    Pass with Reader

--- demo-jpa/src/main/java/demo/books/controller/ReadingListController.java
+++ demo-jpa/src/main/java/demo/books/controller/ReadingListController.java
@@ -3,7 +3,6 @@ package demo.books.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +28,8 @@ public class ReadingListController {
 	}
 
 	@RequestMapping(value = "/{reader}", method = RequestMethod.GET)
-	public String readersBooks(@PathVariable("reader") String reader, Model model) {
+//	public String readersBooks(@PathVariable("reader") String reader, Model model) {
+	public String readerBooks(Reader reader, Model model) {
 		List<Book> readingList = readingListRepository.findByReader(reader);
 		if (readingList != null) {
 			model.addAttribute("books", readingList);
@@ -40,9 +40,9 @@ public class ReadingListController {
 	}
 
 	@RequestMapping(value = "/{reader}", method = RequestMethod.POST)
-	public String addToReadingList(@PathVariable("reader") String reader, Book book) {
+	public String addToReadingList(Reader reader, Book book) {
 		book.setReader(reader);
 		readingListRepository.save(book);
-		return "redirect:/readingList";
+		return "redirect:/readingList/{reader}";
 	}
 }
--- demo-jpa/src/main/java/demo/books/repositories/ReadingListRepository.java
+++ demo-jpa/src/main/java/demo/books/repositories/ReadingListRepository.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import demo.books.entities.Book;
+import demo.security.entity.Reader;
 
 public interface ReadingListRepository extends JpaRepository<Book, Long> {
 	List<Book> findByReader(String reader);
+
+	List<Book> findByReader(Reader reader);
 }
